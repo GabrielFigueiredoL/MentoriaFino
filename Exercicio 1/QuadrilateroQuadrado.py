b5f26f431a78ffd62109f3040588a5dbb0a6bd63
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2025-03-28 22:34:08 -0300

    Adicionar teste de ângulos retos

--- Exercicio 1/QuadrilateroQuadrado.py
+++ Exercicio 1/QuadrilateroQuadrado.py
@@ -5,9 +5,10 @@ def quadrilateroQuadrado(lados, angulos):
   if len(lados) != 4 or len(angulos) != 4:
     return False
   
+  testeAngulosReto = sum(angulos) == 360
   testeLados = len(set(lados)) == 1
   testeAngulos = len(set(angulos)) == 1
   
-  return testeLados and testeAngulos
+  return testeLados and testeAngulos and testeAngulosReto
 
 print(quadrilateroQuadrado(ladosQuadrilatero, angulosQuadrilatero))
